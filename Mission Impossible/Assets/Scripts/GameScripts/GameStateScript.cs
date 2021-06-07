79a916af144cd5b694a9b0c162f0cbdfc012e695
Hoang <hoang.bach@outlook.de>
2021-06-07 13:07:55 +0200

    temporary commit

---
+++ Mission Impossible/Assets/Scripts/GameScripts/GameStateScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Script to organize the state of the game, score etc.
+ **/
+public class GameStateScript : MonoBehaviour
+{
+    public int killScore = 0;
+    public int bonusScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
